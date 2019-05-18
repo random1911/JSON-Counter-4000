521c306bf8819b7ceffc213d562e859297036b78
Alexander Ivanov <ivanov@cindicator.com>
2019-05-18 23:50:21 +0300

    Updates

---
+++ precache-manifest.ca404f2eaf8dcf1d4e277e7758410a5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "012aa1d58686fa7082c3",
+    "url": "/JSON-Counter-4000/static/js/runtime~main.7cb7e8fc.js"
+  },
+  {
+    "revision": "943f19abefbc57b54725",
+    "url": "/JSON-Counter-4000/static/js/main.cf4a4bbd.chunk.js"
+  },
+  {
+    "revision": "c4ad91e5a132d80c7235",
+    "url": "/JSON-Counter-4000/static/js/2.61ccb079.chunk.js"
+  },
+  {
+    "revision": "943f19abefbc57b54725",
+    "url": "/JSON-Counter-4000/static/css/main.e8afd382.chunk.css"
+  },
+  {
+    "revision": "17ac07a765f004fbbb2c2fcb83050a33",
+    "url": "/JSON-Counter-4000/index.html"
+  }
+];
